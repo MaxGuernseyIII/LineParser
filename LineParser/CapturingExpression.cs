057de42061318a8d7af8b8453fdea58da1e2a921
Max Guernsey, III <max@hexsw.com>
2026-01-07 12:42:39 -0800

    .

---
+++ LineParser/CapturingExpression.cs
@@ -0,0 +1,10 @@
+﻿namespace LineParser;
+
+public class CapturingExpression(Expression ToCaptureExpression) : Expression
+{
+  public IEnumerable<Match> GetMatchesAtBeginningOf(string ToMatch, Matcher Reentry, MatchExecutionContext Context)
+  {
+    return ToCaptureExpression.GetMatchesAtBeginningOf(ToMatch, Reentry, Context)
+      .Select(M => M with {Captured = [M.Matched]});
+  }
+}
